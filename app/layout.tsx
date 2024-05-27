bfa85d7d0eadf4039a9892ed494d3b337a078351
dtorres <diegotorre5nz@gmail.com>
2024-05-28 11:43:14 +1200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'Dashboard Acme',
+  metadataBase: new URL('https://acme.com/'),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
