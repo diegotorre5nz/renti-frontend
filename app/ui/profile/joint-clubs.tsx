51b1ddcb8af8f45f02e2b7d760727fd934e5b292
dtorres <diegotorre5nz@gmail.com>
2024-06-11 17:20:08 +1200

    feat:Add streaming componenets

--- app/ui/profile/joint-clubs.tsx
+++ app/ui/profile/joint-clubs.tsx
@@ -2,12 +2,9 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { Club } from '@/app/lib/definitions';
-export default async function JointClubs({
-  clubs,
-}: {
-  clubs: Club[];
-}) {
+import { fetchJointClubs } from '@/app/lib/data';
+export default async function JointClubs () {
+  const clubs = await fetchJointClubs();
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
